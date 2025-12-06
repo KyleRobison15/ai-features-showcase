5341e6aa47d4156d996b0b2fd6b0c7344ce96c54
KyleRobison15 <kylerobison15@gmail.com>
2025-12-06 07:17:43 -0700

    Manage OpenAI API Key

---
+++ packages/server/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+// This function from the dotenv library goes into our .env file and loads each one as an environment variable for us automatically
+    // Without the dotenv library, we would have to create our env vars manually on the command line before running our server every single time
+// Needs to be the first line in your module so the env vars are loaded before anything else happens!
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
