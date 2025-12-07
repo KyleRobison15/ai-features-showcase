b7947f4ee7a41a946c661aea80389470a115bf43
KyleRobison15 <kylerobison15@gmail.com>
2025-12-07 09:57:32 -0700

    Extract our API routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,31 +1,25 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 // This function from the dotenv library goes into our .env file and loads each one as an environment variable for us automatically
 // Without the dotenv library, we would have to create our env vars manually on the command line before running our server every single time
 // Needs to be the first line in your module so the env vars are loaded before anything else happens!
 dotenv.config();
 
-// This is what we will use to build our API endpoints and handle requests and responses
+// Create our "app" in express
 const app = express();
 
-// Tells express to automatically parse JSON objects from our request body
+// Tells express to automatically parse JSON objects from our request body. Without this, request.body will be undefined.
 // Returns a middleware function that gets executed before passing the request to our request handler
 app.use(express.json());
 
+// The router is what we will use to build our API endpoints and handle requests and responses
+// We define our router and give it our endpoints in our routes.ts file
+// Each route has a reference to a controller function
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
---
+++ packages/server/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { chatController } from './controllers/chat.controller';
+
+const router = express.Router();
+
+// In real world projects, we shouldn't have the actual route handlers here, we should just have a reference to a function inside a controller
+router.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
+});
+
+// In real world projects, we shouldn't have the actual route handlers here, we should just have a reference to a function inside a controller
+router.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World!' });
+});
+
+// In real world projects, this is how our route should look!
+router.post('/api/chat', chatController.sendMessage);
+
+export default router;
