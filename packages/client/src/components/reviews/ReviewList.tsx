7b4b7a94b07fd452777d0277a164713ed1a91db6
KyleRobison15 <kylerobison15@gmail.com>
2025-12-14 11:24:06 -0700

    Implement react tanstack query to manage state, caching and retries

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/set-state-in-effect */
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import StarRating from './StarRating';
+import { useQuery } from '@tanstack/react-query';
 
 type Props = {
   productId: number;
@@ -22,28 +22,29 @@ type GetReviewsResponse = {
 };
 
 const ReviewList = ({ productId }: Props) => {
-  const [reviewData, setReviewData] = useState<GetReviewsResponse>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+  // The useQuery hook from tanstack allows us to:
+  // 1. Easily manage our state variables
+  // 2. Cache results to avoid unecessary API calls
+  // 3. Automatically handle retries when there is an error calling the API
+  const {
+    data: reviewData,
+    error,
+    isLoading,
+  } = useQuery<GetReviewsResponse>({
+    // The queryKey property is what tanstack uses to lookup data in its built in cache
+    // With this setup, the unique combo of reviews and productId will be used for looking up data in the cache
+    queryKey: ['reviews', productId],
+
+    // The queryFn property is the function that tanstack will use to retrieve the data from the backend when it needs to
+    queryFn: () => fetchReviews(),
+  });
 
   const fetchReviews = async () => {
-    try {
-      setIsLoading(true);
-      const { data } = await axios.get<GetReviewsResponse>(
-        `/api/products/${productId}/reviews`
-      );
-      setReviewData(data);
-    } catch (error) {
-      console.error(error); // Use a logging utility in real world app (like Sentry)
-      setError('Could not fetch the reviews. Try again.');
-    } finally {
-      setIsLoading(false);
-    }
+    const { data } = await axios.get<GetReviewsResponse>(
+      `/api/products/${productId}/reviews`
+    );
+    return data;
   };
-
-  useEffect(() => {
-    fetchReviews();
-  }, []);
 
   if (isLoading) {
     return (
@@ -60,7 +61,7 @@ const ReviewList = ({ productId }: Props) => {
   }
 
   if (error) {
-    return <p className="text-red-500">{error}</p>;
+    return <p className="text-red-500">Could not fetch reviews. Try again.</p>;
   }
 
   return (
