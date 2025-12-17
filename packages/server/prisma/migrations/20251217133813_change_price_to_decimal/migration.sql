cbb842d7f497262d9b38e00150d1c474a9cb8dc8
KyleRobison15 <kylerobison15@gmail.com>
2025-12-17 07:00:02 -0700

    Update price from float to decimal in DB

---
+++ packages/server/prisma/migrations/20251217133813_change_price_to_decimal/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+-- Note: This migration was already applied to the database
+-- This file documents the change that occurred
+ALTER TABLE `products` MODIFY `price` DECIMAL(10,2) NOT NULL;
